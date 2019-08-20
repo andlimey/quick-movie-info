dfbb8059690b4fa3798ad0835b027e4969ef19db
andlimey <andychan1451@gmail.com>
2019-08-20 20:51:04 +0800

    Add telegram bot to utilise the movie scraper

---
+++ movies_scraper.py
@@ -0,0 +1,90 @@
+from requests import get
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def get_search_results(movie_title):
+    # Creates an instance of Chrome and makes the request.
+    driver = webdriver.Chrome(r"C:\Users\Andy\Downloads\chromedriver.exe")
+    driver.get("https://www.rottentomatoes.com/")
+
+    # Searches for the search bar and enters the user's input.
+    search_form = driver.find_element_by_class_name("form-control")
+    search_form.send_keys(movie_title)
+    button = driver.find_element_by_id("fullscreen-search-desktop-search-btn")
+    button.click()
+
+    # Parses the result of the search.
+    results_html_soup = BeautifulSoup(driver.page_source, features="html.parser")
+    movies_section = results_html_soup.find("section", id="movieSection")
+    results_list = movies_section.find_all("li", class_="bottom_divider clearfix")
+
+    # Stores the results in a dictionary for use later.
+    movies_dictionary = {}
+    for result in results_list:
+        result_details = result.find("div", class_="details")
+        movie_name = result_details.a.text.strip()
+        movie_year = result_details.find("span", class_="movie_year")
+        if movie_year == None:
+            movie_year == ""
+            movie_name_and_year = movie_name
+        else:
+            movie_year = movie_year.text.strip()
+            movie_name_and_year = movie_name + " " + movie_year
+
+        movie_slug = result_details.a['href']
+        movies_dictionary[movie_name_and_year] = movie_slug
+
+    driver.quit()
+    return movies_dictionary
+
+def get_movie_info(movie_slug):
+    url = "https://www.rottentomatoes.com{}".format(movie_slug)
+
+    response = get(url)
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Extracts relevant data from the movie page
+    movie_synopsis = html_soup.find("div", id="movieSynopsis").text.strip()
+
+    movie_info_div = html_soup.find("div", class_="mop-ratings-wrap score_panel js-mop-ratings-wrap")
+
+    title = movie_info_div.h1.text.strip()
+    consensus = movie_info_div.p.text.strip()
+
+    tomato_ratings_div = movie_info_div.find("div", class_="mop-ratings-wrap__half")
+    tomato_meter = tomato_ratings_div.find("span", class_="mop-ratings-wrap__percentage")
+
+    if tomato_meter == None:
+        tomato_meter = "No reviews available"
+        tomato_total_count = "N.A"
+    else:
+        tomato_meter = tomato_meter.text.strip()
+        tomato_total_count = tomato_ratings_div.small.text.strip()
+
+    audience_ratings_div = movie_info_div.find("div", class_="mop-ratings-wrap__half audience-score")
+    audience_score = audience_ratings_div.find("span", class_="mop-ratings-wrap__percentage")
+
+    if audience_score == None:
+        audience_score = "Not available"
+        audience_verified_ratings = "N.A"
+    else:
+        audience_score = audience_score.text.strip()
+        audience_verified_ratings = audience_ratings_div.strong.text[17:].strip()
+    
+    info_dictionary = {}
+    info_dictionary["url"] = url
+    info_dictionary["title"] = title
+    info_dictionary["movie_synopsis"] = movie_synopsis
+    info_dictionary["consensus"] = consensus
+    info_dictionary["tomato_meter"] = tomato_meter
+    info_dictionary["tomato_total_count"] = tomato_total_count
+    info_dictionary["audience_score"] = audience_score
+    info_dictionary["audience_verified_ratings"] = audience_verified_ratings
+
+    return info_dictionary
+
+# def main():
+#     movie_title = input("Enter Movie Name: ")
+#     movies_dictionary = get_search_results(movie_title)
+#     get_movie_info(movies_dictionary)
---
+++ quick-movie-info-bot.py
@@ -0,0 +1,87 @@
+from telegram.ext import Updater, Filters
+from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from collections import OrderedDict
+import logging
+import movies_scraper
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+updater = Updater(token="")
+dispatcher = updater.dispatcher
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Hello. I am bot that gives bite-sized information regarding movies.\nType /info to find out how to use me!")
+
+def info(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="1. Type /find\n2. Enter movie name. E.g. /find Fast and Furious.\n3. Select the movie you're looking for.")
+
+def reply(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Please use the /find command to search for movies")
+
+def find(bot, update):
+    chat_id = update.message.chat_id
+    # We want the message after /find.
+    user_message = update.message.text[5:].strip()
+    if user_message == "":
+        bot.send_message(chat_id=chat_id, text="Please input a movie name after /find")
+    else:
+        bot.send_message(chat_id=chat_id, text="Your query is: {}".format(user_message))
+        bot.send_message(chat_id=chat_id, text="Wait while we retrieve data...")
+        movies_dictionary = get_movies_dictionary(user_message)
+        button_list = []
+        for movie_name in movies_dictionary:
+            movie_slug = movies_dictionary[movie_name]
+            button_list.append(InlineKeyboardButton(movie_name, callback_data=movie_slug))
+        reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
+        bot.send_message(chat_id, text="Click on one of the movies below, or type /find to search for another movie.", reply_markup=reply_markup)
+
+def search_movie_details(bot, update):
+    movie_slug = update.callback_query.data
+    info_dictionary = movies_scraper.get_movie_info(movie_slug)
+    print_information(bot, update, info_dictionary)
+
+def get_movies_dictionary(movie_name):
+    movies_dictionary = OrderedDict(movies_scraper.get_search_results(movie_name))
+    return movies_dictionary
+
+def print_information(bot, update, info_dictionary):
+    title = info_dictionary["title"]
+    movie_synopsis = info_dictionary["movie_synopsis"]
+    consensus = info_dictionary["consensus"]
+    tomato_meter = info_dictionary["tomato_meter"]
+    tomato_total_count = info_dictionary["tomato_total_count"]
+    audience_score = info_dictionary["audience_score"]
+    audience_verified_ratings = info_dictionary["audience_verified_ratings"]
+    url = info_dictionary["url"]
+    bot.send_message(chat_id=update.callback_query.message.chat_id, text="Title: {}".format(title))
+    bot.send_message(chat_id=update.callback_query.message.chat_id, text="Synopsis: {}".format(movie_synopsis))
+    bot.send_message(chat_id=update.callback_query.message.chat_id, text="Critics Consensus: {}".format(consensus))
+    bot.send_message(chat_id=update.callback_query.message.chat_id, text="Tomatometer: {}. Total Count: {}".format(tomato_meter, tomato_total_count))
+    bot.send_message(chat_id=update.callback_query.message.chat_id, text="Audience Score: {}. Number of Verified Ratings: {}".format(audience_score, audience_verified_ratings))
+    bot.send_message(chat_id=update.callback_query.message.chat_id, text="For more information, go to {}".format(url))
+
+def build_menu(buttons,
+               n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons)
+    if footer_buttons:
+        menu.append(footer_buttons)
+    return menu
+
+# Adds handlers and register in the dispatcher.
+start_handler = CommandHandler("start", start)
+info_handler = CommandHandler("info", info)
+find_handler = CommandHandler("find", find)
+callback_handler = CallbackQueryHandler(search_movie_details)
+reply_handler = MessageHandler(Filters.text, reply)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(info_handler)
+dispatcher.add_handler(find_handler)
+dispatcher.add_handler(callback_handler)
+dispatcher.add_handler(reply_handler)
+
+updater.start_polling()
+updater.idle()
